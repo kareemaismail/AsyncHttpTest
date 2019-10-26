1253d4e66d7890acdffb028cadb5b181c860a72a
Kareema Ismail <kismail@nxgn.co.za>
2019-10-26 12:00:16 +0200

    Created a test api

---
+++ WebApplication1/MockDb.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+
+namespace WebApplication1
+{
+    public class MockDb
+    {
+        public string Save()
+        {
+
+            Thread.Sleep(5000);
+            if (!Dictionary.ContainsKey(AddCount))
+            {
+                Dictionary.Add(AddCount, AddCount);
+            }
+            else
+            {
+                Dictionary[AddCount] = AddCount;
+            }
+            AddCount++;
+
+            return AddCount.ToString();
+        }
+
+        public static Dictionary<int, int> Dictionary = new Dictionary<int, int>();
+        public static int AddCount = 1;
+    }
+}
---
+++ WebApplication1/TestController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace WebApplication1
+{
+    public class TestController : ApiController
+    {
+        // GET api/<controller>
+        public string Get()
+        {
+            return new MockDb().Save();
+        }
+    }
+}
